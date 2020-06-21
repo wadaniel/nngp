1bf942eee713376c0769cc624c3369c711f838f9
wadaniel <wadaniel@ethz.ch>
2020-06-21 14:44:57 +0200

    added runs

--- scripts/run-experiment1-case2.sh
+++ scripts/run-experiment1-case2.sh
@@ -4,7 +4,15 @@
 
 pushd ..
 
+python run_experiments.py --num_train=64 --num_eval=1000 --hparams='nonlinearity=relu,depth=5,weight_var=2.97,bias_var=0.28' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=False --dataset='cifar'
+
+
 python run_experiments.py --num_train=100 --num_eval=1000 --hparams='nonlinearity=relu,depth=5,weight_var=2.97,bias_var=0.28' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=False --dataset='cifar'
+
+python run_experiments.py --num_train=200 --num_eval=1000 --hparams='nonlinearity=relu,depth=5,weight_var=2.97,bias_var=0.28' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=False --dataset='cifar'
+
+python run_experiments.py --num_train=256 --num_eval=1000 --hparams='nonlinearity=relu,depth=5,weight_var=2.97,bias_var=0.28' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=False --dataset='cifar'
+
 
 popd
 
