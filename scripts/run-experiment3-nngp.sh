d1f0996eb437e2291ebf35e892659abc72ab9a79
Mathieu Chevalley <mchevalley@lo-login-01.leonhard.ethz.ch>
2020-06-24 19:47:35 +0200

    merge

--- scripts/run-experiment3-nngp.sh
+++ scripts/run-experiment3-nngp.sh
@@ -4,7 +4,7 @@ use_precomputed=False
 
 pushd ..
 
-python run_experiments.py --num_train=100 --num_eval=10000 --hparams='nonlinearity=relu,depth=5,weight_var=1.60,bias_var=1.07' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=${use_precomputed} --dataset='cifar'
+python run_experiments.py --num_train=10000 --num_eval=5000 --hparams='nonlinearity=relu,depth=5,weight_var=1.60,bias_var=1.07' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=${use_precomputed} --dataset='cifar'
 
 popd
 
