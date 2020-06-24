4df479a2963532fca6caf0c803199b8a97a5a63e
wadaniel <wadaniel@ethz.ch>
2020-06-24 16:58:31 +0200

    exp 3 script

--- scripts/run-experiment3-nngp.sh
+++ scripts/run-experiment3-nngp.sh
@@ -6,7 +6,7 @@
 
 pushd ..
 
-python run_experiments.py --num_train=10000 --num_eval=10000 --hparams='nonlinearity=relu,depth=5,weight_var=1.60,bias_var=1.07' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=False --dataset='cifar'
+python run_experiments.py --num_train=10000 --num_eval=10000 --hparams='nonlinearity=relu,depth=5,weight_var=1.60,bias_var=1.07' --n_gauss=501 --n_var=501 --n_corr=500 --max_gauss=10 --use_precomputed_grid=True --dataset='cifar'
 
 popd
 
