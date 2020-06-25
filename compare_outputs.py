d827721371c50622dabf1c7476e660321212333b
wadaniel <wadaniel@ethz.ch>
2020-06-25 16:22:27 +0200

    comparison scrip

--- compare_outputs.py
+++ compare_outputs.py
@@ -1,21 +1,24 @@
 import argparse
+import csv
 import numpy as np
-#from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error
 
 def computeMSOD(outNN, outNNGP):
     return mean_squared_error(outNN, outNNGP)
 
 def readFile(fname):
-    f = np.load(fname)
-    print(f)
+    f = np.load("./experiments/" + fname)
     return f
 
-def writeMSOD(msod):
-    print(msod)
-
+def writeMSOD(fname, nnname, msod):
+    with open(fname, 'a') as file:
+        fieldnames = ['nn_name', 'val']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow({'nn_name': nnname, 'val': msod})
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--fout', required=True, type=str)
     parser.add_argument('--fnn', required=True, type=str)
     parser.add_argument('--fnngp', required=True, type=str)
     opt = parser.parse_args()
@@ -24,5 +27,5 @@ if __name__ == '__main__':
     outnn   = readFile(opt.fnn)
     
     msod = computeMSOD(outnn, outnngp)
-    writeMSOD(msod)
+    writeMSOD(opt.fout, opt.fnn, msod)
 
